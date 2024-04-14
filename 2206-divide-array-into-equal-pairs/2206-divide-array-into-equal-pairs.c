8882bbbe58e894e4e27214990e420dcde8ea3d23
Mariam Gado <74381393+MariamGado0@users.noreply.github.com>
2024-04-14 21:10:51 +0200

    Time: 0 ms (100.00%), Space: 6.3 MB (32.73%) - LeetHub

--- 2206-divide-array-into-equal-pairs/2206-divide-array-into-equal-pairs.c
+++ 2206-divide-array-into-equal-pairs/2206-divide-array-into-equal-pairs.c
@@ -1,26 +1,17 @@
-
-
-
-
-
-bool divideArray(int* nums, int numsSize)
-{
-  
-    for (int i = 0; i < numsSize; i++)
-    {
-            int count = 0;
-            for (int j = 0; j < numsSize; j++) 
-            {
-                if (nums[i] == nums[j]) 
-                {
-                    count++;
-                }
-            }
-
-
-            if (count % 2 != 0) {
-                return false; 
-            }
+bool divideArray(int* nums, int numsSize) {
+    int count[501] = {0}; // Using 501 since nums[i] can range from 1 to 500
+    
+    // Count the occurrences of each number in nums
+    for (int i = 0; i < numsSize; ++i) {
+        count[nums[i]]++;
+    }
+    
+    // Check if each number appears even number of times
+    for (int i = 1; i <= 500; ++i) {
+        if (count[i] % 2 != 0) {
+            return false;
         }
-        return true; 
+    }
+    
+    return true;
 }
